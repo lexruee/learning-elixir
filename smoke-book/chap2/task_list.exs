ec4d0c5b472953418ec7d0d78190d20bab1780c4
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2019-03-15 23:43:30 +0100

    Add TaskList example

---
+++ smoke-book/chap2/task_list.exs
@@ -0,0 +1,17 @@
+#!/usr/bin/env elixir
+
+defmodule TaskList do
+  @file_name "task_list.md"
+
+  def add(task_name) do
+    task = "[ ] " <> task_name <> "\n"
+    File.write(@file_name, task, [:append])
+  end
+
+  def show_list do
+    File.read(@file_name)
+  end
+end
+
+TaskList.add("Learning Elixir")
+IO.inspect TaskList.show_list
