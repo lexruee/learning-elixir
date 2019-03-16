5273f43bac592b272e1767bc25e1d2cb89bc098d
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2019-03-16 12:17:16 +0100

    Add ability modifier example

---
+++ smoke-book/chap3/ability_modifier.exs
@@ -0,0 +1,8 @@
+#!/usr/bin/env elixir
+
+user_input = IO.gets "Write your ability score:\n"
+{ability_score, _} = Integer.parse(user_input)
+
+ability_modifier = (ability_score - 10) / 2
+IO.puts "Your ability score is #{ability_modifier}"
+
