f80abd02a726a920faa47462815d6a3afb0df4d8
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2019-03-21 19:27:59 +0100

    Add factorial stream example

---
+++ smoke-book/chap5/factorial.exs
@@ -0,0 +1,12 @@
+#!/usr/bin/env elixir
+
+defmodule Factorial do
+  def of(0), do: 1
+  def of(n) when n > 0 do
+    Stream.iterate(1, &(&1 + 1))
+    |> Enum.take(n)
+    |> Enum.reduce(&(&1 * &2))
+  end
+end
+
+IO.inspect Factorial.of(5)
